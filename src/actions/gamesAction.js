70eaf04eb37016786d13e9fc19579d046e591a7b
Aurelian <70028912+aurelianvoinea6@users.noreply.github.com>
2022-04-11 21:08:49 +0200

    fetching other games

--- src/actions/gamesAction.js
+++ src/actions/gamesAction.js
@@ -1,16 +1,20 @@
 import axios from 'axios';
-import { popularGamesUrl } from '../api';
+import { popularGamesUrl,upcomingGamesUrl,newGamesUrl } from '../api';
 
 //action creator
 
 
 export const loadGames = () => async (dispatch) => {
     // fetch axios
-    const popularData = await axios.get(popularGamesUrl())
+    const popularData = await axios.get(popularGamesUrl());
+    const newGamesData = await axios.get(newGamesUrl());
+    const upcomingData = await axios.get(upcomingGamesUrl());
     dispatch({
         type: "FETCH_GAMES",
         payload: {
             popular: popularData.data.results,
+            upcoming: upcomingData.data.results,
+            newGames: newGamesData.data.results,
         },
     });
 };
