a062a1ae1f44e4c8bbacd8c51c2385a81e38b1c0
Aurelian <70028912+aurelianvoinea6@users.noreply.github.com>
2022-04-11 19:00:33 +0200

    setting the api, getting the current date and the last year and next year

---
+++ src/api.js
@@ -0,0 +1,44 @@
+// base url
+const base_url = "https://api.rawg.io/api/";
+const key = "1e0f3b6418da4c7a9174b702a5e7a388";
+
+//getting the date 
+const getCurrentMonth = () => {
+    const month = new Date().getMonth() +1;
+    if(month < 10) {
+        return `0${month}`
+    }else{
+        return month;
+    }
+};
+
+const getCurrentDay = () => {
+    const day = new Date().getDay();
+    if(day < 10) {
+        return `0${day}`
+    }else{
+        return day;
+    }
+};
+
+//current day/month/year
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear =`${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+console.log(currentDate);
+
+getCurrentMonth();
+
+//popular games
+
+const popular_games = `games?key=1e0f3b6418da4c7a9174b702a5e7a388&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`
+
+ const popularGamesUrl = () => `${base_url}${popular_games}${key}`
+
+console.log(popularGamesUrl());
+
+ 
