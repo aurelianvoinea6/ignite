08fcc4167e85c83546ccf7ebb1a4853b5a1a939f
Aurelian <70028912+aurelianvoinea6@users.noreply.github.com>
2022-04-20 21:54:01 +0200

    added overflow hidden to the styled game

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React from 'react';
 //components and pages
 import Home from './pages/Home'
 import GlobalStyles from './components/GlobalStyles';
-import {Route} from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 function App() {
  
   return (
     <div className="App">
       <GlobalStyles/>
-      <Route path= {["/game/:id", "/"]}>
-        <Home />
-      </Route>
+        <Route path= {['/game/:id', '/']}>
+          <Home />
+        </Route>
     </div>
   );
 }
