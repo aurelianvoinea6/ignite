13167c3201b5e2820feb8a58cec03a2c3bae6ed2
Aurelian <70028912+aurelianvoinea6@users.noreply.github.com>
2022-04-20 23:11:30 +0200

    removed the route for test

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import React from 'react';
 //components and pages
 import Home from './pages/Home'
 import GlobalStyles from './components/GlobalStyles';
-import { Route } from 'react-router-dom';
+
 
 function App() {
  
   return (
     <div className="App">
       <GlobalStyles/>
-        <Route path= {['/game/:id', '/']}>
-          <Home />
-        </Route>
+        <Home />
     </div>
   );
 }
